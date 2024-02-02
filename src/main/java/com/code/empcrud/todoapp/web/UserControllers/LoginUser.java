e85bf323f6564703664b048370749cd9b457a1ce
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-02-02 14:37:54 +0200

    Login User Controller

---
+++ src/main/java/com/code/empcrud/todoapp/web/UserControllers/LoginUser.java
@@ -0,0 +1,47 @@
+package com.code.empcrud.todoapp.web.UserControllers;
+
+import com.code.empcrud.todoapp.dao.LoginDao;
+import com.code.empcrud.todoapp.model.LoginBean;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet( name = "Login-user", value = "/login-user")
+public class LoginUser extends HttpServlet {
+    LoginDao loginDao;
+
+    public void init(){
+        this.loginDao = new LoginDao();
+    }
+
+    public void authenticate(HttpServletRequest request, HttpServletResponse res) throws IOException, ServletException{
+        String username= request.getParameter("uname");
+        String password =  request.getParameter("passwd");
+        LoginBean loginBean = new LoginBean();
+
+        loginBean.setUsername(username);
+        loginBean.setPassword(password);
+
+        boolean result = loginDao.validate(loginBean);
+        if(result)
+        {
+//            HttpSession session = request.getSession();
+//            session.setAttribute("username", username);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("todo-list.jsp");
+            dispatcher.forward(request, res);
+        }
+        else {
+            res.sendRedirect("login.jsp");
+        }
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
+        authenticate(req, res);
+    }
+}
