35f7047f91cbd645932a738544256d7c277139ab
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-02-02 14:39:26 +0200

    Todo Data Access Object Interface

--- src/main/java/com/code/empcrud/todoapp/dao/TodoDao.java
+++ src/main/java/com/code/empcrud/todoapp/dao/TodoDao.java
@@ -1,4 +1,15 @@
 package com.code.empcrud.todoapp.dao;
 
+import com.code.empcrud.todoapp.model.Todo;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface TodoDao {
+    boolean insertTodo(Todo todo) throws SQLException;
+    Todo selectTodo(long todoId);
+    List<Todo> selectAllTodos();
+    boolean deleteTodo(int id) throws  SQLException;
+
+    boolean updateTodo(Todo todo) throws  SQLException;
 }
