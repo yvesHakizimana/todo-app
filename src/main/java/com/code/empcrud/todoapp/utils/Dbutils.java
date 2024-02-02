ce6ddb72350fa4bb8a39ab53464b198b494ded4f
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-02-02 11:35:39 +0200

    Database connection  utility

--- src/main/java/com/code/empcrud/todoapp/utils/Dbutils.java
+++ src/main/java/com/code/empcrud/todoapp/utils/Dbutils.java
@@ -1,4 +1,36 @@
 package com.code.empcrud.todoapp.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Dbutils {
+
+    private static String jdbcUrl = "jdbc:postgresql://localhost:5432/webapp";
+    private static String jdbcUsername = "postgres";
+    private static String jdbcPassword = "P0S1tiv@!";
+
+    public Connection connectToDb(){
+        Connection conn = null;
+        try{
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword);
+            if(conn != null)
+                System.out.println("Successfully connected to the daatabase");
+        } catch(SQLException ex){
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("ErrorCode: " + ex.getErrorCode());
+            System.out.println("Message: " + ex.getMessage());
+        } catch(ClassNotFoundException ex){
+            throw new RuntimeException("ClassNotFoundException");
+        }
+
+        return conn;
+    }
+
+    public static void main(String[] args){
+        Dbutils dbutils =  new Dbutils();
+        dbutils.connectToDb();
+    }
+
 }
