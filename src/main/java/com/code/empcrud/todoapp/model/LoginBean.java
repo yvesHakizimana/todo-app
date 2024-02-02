e98cf308f3b57fc6d0e7f37023a0fabc57252f64
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-02-02 14:37:11 +0200

    Login Bean Model

--- src/main/java/com/code/empcrud/todoapp/model/LoginBean.java
+++ src/main/java/com/code/empcrud/todoapp/model/LoginBean.java
@@ -1,4 +1,29 @@
 package com.code.empcrud.todoapp.model;
 
-public class LoginBean {
+import java.io.Serializable;
+
+public class LoginBean implements Serializable {
+
+    private static  final long serialVersionUID = 1L;
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
 }
